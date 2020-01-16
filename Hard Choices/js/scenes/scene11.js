ac9e041a04e12e42fc41a89354d6322d5453b10b
Ryken Thompson <rykenthompson@icloud.com>
2020-01-16 09:13:12 -0800

    Update scene11.js

--- Hard Choices/js/scenes/scene11.js
+++ Hard Choices/js/scenes/scene11.js
@@ -23,10 +23,40 @@ var scene11 = new Phaser.Class({
         this.background = this.add.sprite(0,0, 'bg_2');
         this.background.setOrigin(0,0);
         this.background.setScale(0.65);
+        
+        this.highpay = this.add.sprite(300, 200, 'text38');	
+        this.highpay.setScale(0.7);
+        this.highpay.visible = false;
+
+        this.lowpay = this.add.sprite(300, 200, 'text39');	
+        this.lowpay.setScale(0.7);
+        this.lowpay.visible = false;
+
+        this.isStart = true;
+
+        this.startTime = 0;
     },
 
     update: function (time, delta) {
-        
+        if (this.isStart) {
+            this.isStart = false;
+            if (highPayJob) {
+                this.highpay.visible = true;
+                savings += 700;
+                this.startTime = time;
+            } else {
+                this.lowpay.visible = true;
+                savings += 600;
+                this.startTime = time;
+            }
+
+            this.startTime = time;
+        }
+
+        if (!this.isStart && (time - this.startTime) > 3000) {
+            nextScene = 'scene6';
+            this.scene.start('rentScene');
+        }
+
     }
 });
-
